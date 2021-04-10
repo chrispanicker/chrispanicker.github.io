a10c083149347637337118da3d7eaff825e23928
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2021-04-10 15:39:03 -0400

    airtable integration start

--- windows/main.js
+++ windows/main.js
@@ -3,17 +3,23 @@ const answerArray = [];
 
 var everywindow = document.getElementById('every_window');
 
+//calls questionOne function on load
+window.onload = questionOne;
+
 //defining choices for question
 function questionOne(){
- 	let choiceOne = Math.floor(Math.random()*100)
- 	let choiceTwo = Math.floor(Math.random()*100)
+    let choiceOne = Math.floor(Math.random()*101)
+    let choiceTwo = Math.floor(Math.random()*101)
+    let answerChoice;
 
-//if rng causes two of the same number, do it again
+    //if rng causes two of the same number, do it again
  	if (choiceTwo == choiceOne){
- 		let choiceTwo = Math.floor(Math.random()*100)
- 	}
- 	console.log(choiceOne);
- 	console.log(choiceTwo);
+        choiceTwo = choiceTwo-1;
+        console.log('duplicate!')
+ 	} 
+
+    console.log(choiceOne);
+    console.log(choiceTwo);
 
 	if (every_window) {
         // define and create elements
@@ -21,19 +27,31 @@ function questionOne(){
 		let btn1 = document.createElement("button")
 		let btn2 = document.createElement("button")
 
+ 
+         //function that turns value into string
+        String(choiceOne);
+        String(choiceTwo);
+
         // Set innerHTML of the elements
         btn1.innerHTML = choiceOne
         btn2.innerHTML = choiceTwo
 
-        //onclick function
-        btn1.addEventListener("click", pushToArray);
-        btn2.addEventListener("click", pushToArray);        
+        //click function
+        btn1.addEventListener("click", pushToArray1)
+        btn2.addEventListener("click", pushToArray2)      
+
+
         // append them to the parent div
         document.getElementById('every_window').append(parent);
 		parent.append(btn1, btn2)
+    
+    function pushToArray1(){
+        answerArray.push(choiceOne);
+        console.log(answerArray);
+        }
+    function pushToArray2(){
+        answerArray.push(choiceTwo);
+        console.log(answerArray);
+        }
     }
 };
-
-function pushToArray(){
-
-}
--- windows/script.js
+++ windows/script.js
@@ -1,33 +1,49 @@
 var Airtable = require('airtable');
 var base = new Airtable({apiKey: 'key3wySYYU0tXKf12'}).base('appwWvc8kL4LpURS5');
 
-let every_window = document.getElementById('every_window');
+let fullArray = [];
 
 base('Table 1').select({
-   maxRecords: 100,
+   maxRecords: 101,
    view: "Grid view"
 }).eachPage(function page(records, fetchNextPage) {
     // This function (`page`) will get called for each page of records.
     records.forEach(function(record) {
       if (every_window) {
 
+        fullArray.push(record);
+        console.log(fullArray);
+
+
          // define and create elements
-         let el = document.createElement('li');
-         let p = document.createElement('p');
-         let h1 = document.createElement('h1');
+         let parent1 = document.createElement('div');
+         let parent2 = document.createElement('div')
          let img = document.createElement('img');
 
-         // Set innerHTML of the elements
+      
+         /*// Set innerHTML of the elements
          h1.innerHTML = record.get('Feeling');
          p.innerHTML = record.get('Shape');
-         img.src = record.fields.Attachments.url;
-         console.log(h1.innerHTML)
-         console.log(p.innerHTML)
-         // append them to the list
-         every_window.append(el);
-         el.append(img, h1, p);
+         img.src = record.fields.Attachments[0].url;
+
+        //img styling
+        img.style.height = "40vw";
+        img.style.width = "40vw";
+        img.style.objectFit = "cover";
+
+        //change img shape based on the Shape record 
+        if (record.get('Shape') == "circular"){ 
+            img.style.borderRadius= "40vw";
+            console.log("gotcha");
+        }
+
+        // append them to the list
+        document.getElementById('every_window').append(parent);
+        console.log(record)
+        parent.append(img, h1, p);*/
       }
     });
+    
 
     fetchNextPage();
 
