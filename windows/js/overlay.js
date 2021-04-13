f5cde861a2799586b06e0938a93f6ff387599303
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2021-04-13 04:13:02 -0400

    FINAL
    Added Intro/Conclusion,
    Fixed all bugs
    Removed question 4 implementations

--- windows/js/overlay.js
+++ windows/js/overlay.js
@@ -1,16 +1,75 @@
 //array to push word form of numbers into page
 let questionArray = ['one', 'two', 'three', 'four', 'five'];
-let currentQuestion = answerArray.length;
+
+let typeOneText = ['which windows would you prefer to look out from?','which windows do you like?', 
+'which windows tell you a story?', 'which windows did your eye draw to first?', 
+'which windows would not be your first choice?','which windows would you rather break?', ]
+
+let typeOneConc =['you are interested in things that are ', 'you see yourself as ', 
+'you enjoy stories that are ', 'you are feeling ', 
+'you avoid people that are ', "some say you're chaotic, but really, you're ", ]
+
+let typeThreeText = ['which windows best represent you in twenty years?', 
+'which windows make you feel sad?', 'which windows make you want to reevaluate your place in society?', 
+'which windows remind you of home?', 'which windows are you not likely to see today?',
+'which windows talk about you behind your back?', "which windows feel like a place you've been before?"]
+
+let typeThreeConc = ['you think of the future as ', 'you hold on to memories that are ', 
+'you believe life is ', 'you find comfort in things that are ', 'currently, you feel ', 'you are afraid that you are ',
+'you see the world as ']
+
+let typeFourText = ['in one word, describe how these windows make you feel.', 'in one word, how would you describe these windows?',
+'in one word, please enter how these windows look to you.',]
+
+let typeFourConc=['you feel ', 'you are ', 'you think of yourself as ',]
+
+let textNumber;
+let textNumberArray = [];
+let qst;
+let txt;
 
 
+//inserts respective number for question
 function questionNumber(){
-	console.log("got question number")
-	//create elements
-	let qst = document.createElement("p")
-
+	let currentQuestion = answerArray.length;
+	console.log("got question number")	
+	//create element
+	qst = document.createElement("p")
 	// Set innerHTML of the element
-	qst.innerHTML = "question " + questionArray[currentQuestion];
-
+	qst.innerHTML = "question " + questionArray[currentQuestion] + ":";
 	//append to parent
 	document.getElementById('question_number').append(qst);
+};
+
+
+function questionText(){
+	txt = document.createElement('h2');
+	console.log('got question text');
+
+	//rng to choose random question based on type
+	if (currentType == 1){
+	textNumber = Math.floor(Math.random()*typeOneText.length);
+	//getting question from typeOneText
+	txt.innerHTML = typeOneText[textNumber];
+	}
+	if (currentType == 2){
+	textNumber = Math.floor(Math.random()*typeOneText.length);
+	txt.innerHTML = typeOneText[textNumber];
+	}
+	if (currentType == 3){
+	textNumber = Math.floor(Math.random()*typeThreeText.length);
+	txt.innerHTML = typeThreeText[textNumber];
+	}
+	if (currentType == 4){
+	textNumber = Math.floor(Math.random()*typeFourText.length);
+	txt.innerHTML = typeFourText[textNumber];
+	}
+
+	textNumberArray.push({"type": currentType, "question": textNumber});
+	document.getElementById('question').append(txt);
+};
+
+function resetQuestion(){
+	document.getElementById('question_number').innerHTML = '';
+	document.getElementById('question').innerHTML = '';
 }
--- windows/js/script.js
+++ windows/js/script.js
@@ -1,8 +1,9 @@
 var Airtable = require('airtable');
 var base = new Airtable({apiKey: 'key3wySYYU0tXKf12'}).base('appwWvc8kL4LpURS5');
-let typeArray = [questionOne, questionTwo, questionThree, questionFour];
+let typeArray = [questionOne, questionTwo, questionThree,];
 let fullArray = [];
 let answerArray = [];
+let currentType = 0;
 
 base("Table 1").select({maxRecords: 100, view:"Grid view"}).eachPage(gotWindow, gotAllWindows);
 
@@ -21,30 +22,62 @@ function gotWindow(records, fetchNextPage) {
 // call back function that is called when all pages are loaded
 function gotAllWindows(err) {
   console.log("got all windows");
-  questionNumber();
-  rngQuestion();
+  intro();
 }
 
 //function that randomly chooses a question type:
 function rngQuestion(){
+    if (answerArray.length == 5){
+        conclusion();
+        return;
+    }
     let rngIndex = Math.floor(Math.random()*typeArray.length)
     typeArray[rngIndex]();
-    let removeType = typeArray.splice(rngIndex, 1);
-  //  console.log(removeType);
+    questionNumber(); //See overlay.js
+    questionText();
 }
+//----------------------INTRO--------------------------------------------
+function intro(){
+    let p1 = document.createElement('p')
+    let p2 = document.createElement('p')
+    let p3 = document.createElement('p')
+    let end = document.createElement('p')
+    let parent1= document.createElement('div')
+    let parent2= document.createElement('div')
+    let parent3= document.createElement('div')
+    let parent4= document.createElement('div')
+    let gp = document.createElement('div')
+    gp.classList.add('gp')
+    p1.innerHTML = "what do you see when you look at a window?"
+    p2.innerHTML = "do you see the sky?"
+    p3.innerHTML = "or do you see yourself?"
+    end.innerHTML = 'start'
 
+    parent1.classList.add('parent')
+    parent2.classList.add('parent')
+    parent3.classList.add('parent')
+    parent4.classList.add('parent')
+    end.classList.add('end')
+    end.addEventListener('click', ending);
+    document.getElementById('every_window').append(gp);
+    gp.append(parent1, parent2, parent3, parent4,);  
+    parent1.append(p1);
+    parent2.append(p2);
+    parent3.append(p3);
+    parent4.append(end);
 
-
-
-
-
+    function ending(){
+        document.getElementById('every_window').innerHTML = '';    
+        rngQuestion();
+    }
+}
 
 //-------------------------------question type 1 "two choices"---------------------------------------
 
 //defining choices for question
 function questionOne(){
-
-    console.log('questionOne')
+    currentType = 1;
+    console.log('Question Type 1')
     let choiceOne = Math.floor(Math.random()*100)
     let choiceTwo = Math.floor(Math.random()*100)
 
@@ -88,18 +121,23 @@ function questionOne(){
 
     function pushToArray1(){
         answerArray.push(choiceOne);
-        console.log(answerArray);
+        document.getElementById('every_window').innerHTML = '';
+        resetQuestion();
+        rngQuestion();
         }
     function pushToArray2(){
         answerArray.push(choiceTwo);
-        console.log(answerArray);
+        document.getElementById('every_window').innerHTML = '';
+        resetQuestion();
+        rngQuestion();
         }
     }
 };
 //-------------------------------question type 2 "three choices"---------------------------------------
 
 function questionTwo(){
-    console.log('questionTwo')
+    currentType = 2;
+    console.log('Question Type 2')
     let choiceOne = Math.floor(Math.random()*100)
     let choiceTwo = Math.floor(Math.random()*100)
     let choiceThree = Math.floor(Math.random()*100)
@@ -158,30 +196,41 @@ function questionTwo(){
 
     function pushToArray1(){
         answerArray.push(choiceOne);
-        console.log(answerArray);
+        document.getElementById('every_window').innerHTML = '';
+        resetQuestion();
+        rngQuestion();
         }
     function pushToArray2(){
         answerArray.push(choiceTwo);
-        console.log(answerArray);
+        document.getElementById('every_window').innerHTML = '';
+        resetQuestion();
+        rngQuestion();
         }
     function pushToArray3(){
         answerArray.push(choiceThree);
-        console.log(answerArray);
+        document.getElementById('every_window').innerHTML = '';
+        resetQuestion();
+        rngQuestion();
         }
     }
 };
 
 //-------------------------------question type 3 "random image"---------------------------------------
-//NOT WORKING!!! Values are stacking on click
 
 function questionThree(){
-        console.log('questionThree')
-    let img1 = document.createElement("img")
+    currentType = 3;
+    console.log('Question Type 3')
 
-var intervalId = window.setInterval(function(){ 
-    img1.innerHTML = '';
+    let choiceOne;
+    let img1 = document.createElement("img")
+    let imgAmount = 0;
+
+//creating a 3 second interval to generate new images
+var intervalId = window.setInterval(function(){
+
+    img1.removeEventListener("click", pushToArray1);
     img1.id = "imgAnimation"
-    let choiceOne = Math.floor(Math.random()*100)
+    choiceOne = Math.floor(Math.random()*100);
 
     if (every_window) {
         // define and create elements
@@ -191,35 +240,179 @@ var intervalId = window.setInterval(function(){
         String(choiceOne);
 
         // Set innerHTML of the elements
-        img1.innerHTML = choiceOne
+        img1.innerHTML = choiceOne;
 
         //src of images
-        console.log("finding image", choiceOne, fullArray[choiceOne].fields.Attachments[0].url)
-
-        img1.src= fullArray[choiceOne].fields.Attachments[0].url
+        console.log("finding image", choiceOne, fullArray[choiceOne].fields.Attachments[0].url);
+        img1.src= fullArray[choiceOne].fields.Attachments[0].url;
 
         //click event
-        img1.addEventListener("click", pushToArray1)
+        img1.addEventListener("click", pushToArray1);
 
         // append them to the parent div
         document.getElementById('every_window').append(parent1);
-        parent1.append(img1)
+        parent1.append(img1);
+    
+    }
+}, 2000); //end of interval
 
     function pushToArray1(){
         answerArray.push(choiceOne);
-        console.log(answerArray);
-    }
-    }
-}, 3000);
+        clearInterval(intervalId);   
+        document.getElementById('every_window').innerHTML = '';
+        resetQuestion();
+        rngQuestion();
+        }
 };
 
-//-------------------------------question type 4 "click and drag"---------------------------------------
+//-------------------------------question type 4 "input"---------------------------------------
 function questionFour(){
-    let img1 = document.createElement("img")
-    let img2 = document.createElement("img")
-    let img3 = document.createElement("img")
-    let img4 = document.createElement("img")
-    let img5 = document.createElement("img")    
-    let img6 = document.createElement("img")
-    console.log('questionFour')
+    currentType = 4;
+    console.log('Question Type 4')
+    let choiceOne = Math.floor(Math.random()*100);
+    let img1 = document.createElement("img");
+    let form = document.createElement('form');
+    let inp = document.createElement("INPUT");
+    let sub = document.createElement("button");
+    inp.setAttribute("type", "text");
+    inp.setAttribute("placeholder", "test!");
+    sub.setAttribute("type", 'button');
+    sub.innerHTML = "submit"
+    inp.id = "yourText";
+    //onclick run getInput() function
+    sub.addEventListener('click', getInput);
+
+    img1.innerHTML = choiceOne;
+    img1.src=fullArray[choiceOne].fields.Attachments[0].url;
+
+    // append them to the parent div
+    document.getElementById('every_window').append(img1, inp, sub);
+
+    function getInput(){
+        let yourText = document.getElementById('yourText').value;
+        answerArray.push(yourText);
+        resetQuestion();
+        rngQuestion();
+    }
+
+    function pushToArray1(){
+        answerArray.push(choiceOne);
+        document.getElementById('every_window').innerHTML = '';
+        resetQuestion();
+        rngQuestion();
+    }
 };
+//-------------------------------CONCLUSION---------------------------------------
+function conclusion(){
+    //need to associate answers from answerArray with questions
+    console.clear();
+    console.log(textNumberArray);
+    let p1 = document.createElement('p')
+    let p2 = document.createElement('p')
+    let p3 = document.createElement('p')
+    let p4 = document.createElement('p')
+    let p5 = document.createElement('p')
+    let q1 = document.createElement('p')
+    let q2 = document.createElement('p')
+    let q3 = document.createElement('p')
+    let q4 = document.createElement('p')
+    let q5 = document.createElement('p')
+    let parent1 = document.createElement('div')
+    let parent2 = document.createElement('div')
+    let parent3 = document.createElement('div')
+    let parent4 = document.createElement('div')
+    let parent5 = document.createElement('div')
+    let parent6 = document.createElement('div')
+    let gp = document.createElement('div')
+    let end = document.createElement('p')
+    let space = ''
+    parent1.classList.add('parent')
+    parent2.classList.add('parent')
+    parent3.classList.add('parent')
+    parent4.classList.add('parent')
+    parent5.classList.add('parent')
+    parent6.classList.add('end')
+    gp.classList.add('gp')
+    let i;
+    let conc=[];
+    let conc1;
+
+    //FOR LOOPS THAT DETERMINE WHAT QUESTION WAS ASKED
+    for (i=0; i<textNumberArray.length; i++){
+        if(textNumberArray[i].type==1){
+            conc.push(typeOneConc[textNumberArray[i].question])
+        }
+        if(textNumberArray[i].type==2){
+            conc.push(typeOneConc[textNumberArray[i].question])
+        }                                
+        if(textNumberArray[i].type==3){
+            conc.push(typeThreeConc[textNumberArray[i].question])
+        } 
+        if(textNumberArray[i].type==4){
+            conc.push(typeFourConc[textNumberArray[i].question])
+        }     
+    }
+    console.log(conc);
+    //CONCLUSIONS TO BE MADE BY QUESTIONS
+    q1.innerHTML = conc[0];
+    q2.innerHTML = conc[1];
+    q3.innerHTML = conc[2];
+    q4.innerHTML = conc[3];
+    q5.innerHTML = conc[4];
+    //ANSWERS PER QUESTION
+    p1.innerHTML=(fullArray[answerArray[0]].fields.Feeling);
+    p2.innerHTML=(fullArray[answerArray[1]].fields.Feeling);
+    p3.innerHTML=(fullArray[answerArray[2]].fields.Feeling);
+    p4.innerHTML=(fullArray[answerArray[3]].fields.Feeling);
+    p5.innerHTML=(fullArray[answerArray[4]].fields.Feeling); 
+    end.innerHTML="next"; 
+    end.addEventListener('click', nextP);
+
+    document.getElementById('every_window').append(gp);  
+    gp.append(parent1, parent2, parent3, parent4, parent5, parent6);
+    parent1.append(q1,'\xa0', p1, ".");
+    parent2.append(q2,'\xa0', p2,".");
+    parent3.append(q3,"\xa0", p3,".");
+    parent4.append(q4,"\xa0", p4,".");
+    parent5.append(q5,"\xa0", p5,".");
+    parent6.append(end);
+
+    function nextP(){
+        document.getElementById('every_window').innerHTML = '';
+        final();
+    }
+}
+//----------------------FINAL--------------------------------------------
+function final(){
+    let p1 = document.createElement('p')
+    let p2 = document.createElement('p')
+    let p3 = document.createElement('p')
+    let end = document.createElement('p')
+    let parent1= document.createElement('div')
+    let parent2= document.createElement('div')
+    let parent3= document.createElement('div')
+    let parent4= document.createElement('div')
+    let gp = document.createElement('div')
+    gp.classList.add('gp')
+    p1.innerHTML = "but of course, this is all just conjecture."
+    p2.innerHTML = "a distortion of your reality"
+    p3.innerHTML = "like a reflection from a window pane."
+    end.innerHTML = 'end'
+
+    parent1.classList.add('parent')
+    parent2.classList.add('parent')
+    parent3.classList.add('parent')
+    parent4.classList.add('parent')
+    end.classList.add('end')
+    end.addEventListener('click', ending);
+    document.getElementById('every_window').append(gp);
+    gp.append(parent1, parent2, parent3, parent4,);  
+    parent1.append(p1);
+    parent2.append(p2);
+    parent3.append(p3);
+    parent4.append(end);
+
+    function ending(){
+        location.reload();
+    }
+}
