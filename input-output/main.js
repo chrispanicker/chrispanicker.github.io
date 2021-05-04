89a649a83b93e80c6280eb87a49930661c825113
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2021-05-04 02:36:33 -0400

    Prototype Demo

--- input-output/main.js
+++ input-output/main.js
@@ -1,44 +1,55 @@
+let ball;
+let goal;
 
-var px = 50; // Position x and y
-var py = 50;
-var vx = 0.0; // Velocity x and y
-var vy = 0.0;
-var updateRate = 1/60; // Sensor refresh rate
+window.onload = function(){
+    levelOne();
+    getAccel();
+}
 
-function getAccel(){
-    DeviceMotionEvent.requestPermission().then(response => {
-        if (response == 'granted') {
-       // Add a listener to get smartphone orientation 
-           // in the alpha-beta-gamma axes (units in degrees)
-            window.addEventListener('deviceorientation',(event) => {
-                // Expose each orientation angle in a more readable way
-                rotation_degrees = event.alpha;
-                frontToBack_degrees = event.beta;
-                leftToRight_degrees = event.gamma;
-                
-                // Update velocity according to how tilted the phone is
-                // Since phones are narrower than they are long, double the increase to the x velocity
-                vx = vx + leftToRight_degrees * updateRate*2; 
-                vy = vy + frontToBack_degrees * updateRate;
-                
-                // Update position and clip it to bounds
-                px = px + vx*.5;
-                if (px > 98 || px < 0){ 
-                    px = Math.max(0, Math.min(98, px)) // Clip px between 0-98
-                    vx = 0;
-                }
 
-                py = py + vy*.5;
-                if (py > 98 || py < 0){
-                    py = Math.max(0, Math.min(98, py)) // Clip py between 0-98
-                    vy = 0;
-                }
+
+function levelOne(){
+    ball=document.createElement('div');
+    ball.classList.add('ball');
+    document.body.appendChild(ball);
+}
+
+function makeGoal(){
+    gx = Math.floor(Math.random() * 93)
+    gy = Math.floor(Math.random() * 96)
+    goal = document.createElement('div');
+    goal.classList.add('goal');
+    goal.setAttribute('style', "left:" + (gx) + "%;" +
+        "top:" + (gy) + "%;");
+    document.getElementById('main').append(goal);
                 
-                dot = document.getElementsByClassName("indicatorDot")[0]
-                dot.setAttribute('style', "left:" + (px) + "%;" +
-                                              "top:" + (py) + "%;");
-                
-            });
-        }
-    });
+}
+
+function collideCheck(){
+    var goalBounds = goal.getBoundingClientRect();
+    gl = goalBounds.left;
+    gt = goalBounds.top;
+    gr = goalBounds.right;
+    gb = goalBounds.bottom;
+    
+    var ballBounds = ball.getBoundingClientRect();
+    bl = ballBounds.left;
+    bt = ballBounds.top;
+    br = ballBounds.right;
+    bb = ballBounds.bottom;
+    
+    var overlap = !(gr < bl || 
+    gl > br || 
+    gb < bt || 
+    gt > bb);
+    
+    console.log(overlap);
+    if (overlap==true){
+        collision();
+    }
+}
+
+function collision(){
+    main.innerHTML = ('');
+    makeGoal();
 }
